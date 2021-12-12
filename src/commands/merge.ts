04f87c5c0dd057c8a2e43703332e2b5d45043191
José Silas Cardoso Maia Vieira <silascmv@gmail.com>
2021-12-12 17:54:57 -0300

    Adjust

--- src/commands/merge.ts
+++ src/commands/merge.ts
@@ -1,9 +1,13 @@
 import { Command, flags } from '@oclif/command'
-import * as mergeUtils from '../utils/mergeUtils'
+import * as fileUtils from '../utils/fileUtils'
+import MergeFile from '../utils/mergeFile'
+const logger = require('../utils/logUtils');
+
 export default class Merge extends Command {
   static description = 'Merge your metadata from source path to target org path'
 
   targetFolder = '';
+  fileMerging = '';
 
   static examples = [
     `$ sfci merge -t profile -s metadata -d src`,
@@ -21,42 +25,44 @@ export default class Merge extends Command {
   }
 
   async run() {
+    this.log('SFCI - Merge \n')
+
     const { args, flags } = this.parse(Merge);
     this.targetFolder = flags.dir;
 
     switch (flags.type) {
 
       case 'profile':
-        const filesInSource = mergeUtils.getFilesInFolders(flags.source)
-        const filesInTarget = mergeUtils.getFilesInFolders(flags.dir)
-
+        this.log('Starting Merging Profiles \n')
+        const filesInSource = fileUtils.getFilesInFolders(flags.source)
+        const filesInTarget = fileUtils.getFilesInFolders(flags.dir);
         let mapToUpdate = new Map();
         let mapNewProfiles = new Map();
 
-        for (let key of filesInSource.keys()) {
-          if (filesInTarget.has(key)) {
-            mapToUpdate.set(key, filesInTarget.get(key));
+        for (let file of filesInSource.keys()) {
+          if (filesInTarget.has(file)) {
+            mapToUpdate.set(file, filesInTarget.get(file));
           } else {
-            mapNewProfiles.set(key, filesInSource.get(key));
+            mapNewProfiles.set(file, filesInSource.get(file));
           }
         }
 
         // MOVE ONLY NEWPROFILES
         if (mapNewProfiles.size > 0) {
           for (let key of mapNewProfiles.keys()) {
-            mergeUtils.moveFilesToTarget(key, flags.source, flags.dir);
+            fileUtils.moveFilesToTarget(key, flags.source, flags.dir);
           }
         }
 
         // MERGE PERMISSIONS IN SAME FILES
         if (mapToUpdate.size > 0) {
-          for (let key of mapToUpdate.keys()) {
-            this.mergeProfile(key, mapToUpdate.get(key), filesInSource.get(key));
+          for (let arquivo of mapToUpdate.keys()) {
+            this.mergeProfile(arquivo, mapToUpdate.get(arquivo), filesInSource.get(arquivo));
           }
         }
+        this.log('Finish Merging Profiles \n')
 
         break;
-
       default:
         this.log('Unexpected value type')
         break;
@@ -66,50 +72,69 @@ export default class Merge extends Command {
 
 
   mergeProfile(fileName: string, target: any, source: any) {
-    var sourceFile = mergeUtils.convertFile(source);
-    //FIELD PERMISSION MAPS
-    var mapOfFieldObjTarget = mergeUtils.mountMapFieldPermission(target);
-    var mapOfFieldObjSource = mergeUtils.mountMapFieldPermission(source);
-    // USER PERMISSION MAPS
-    var mapUserPermissionTarget = mergeUtils.mountMapUserPermission(target);
-    var mapUserPermissionSource = mergeUtils.mountMapUserPermission(source);
-    // LAYOUT ASSINGMENTS PERMISSION MAPS
-    var mapLayoutAssignmentsTarget = mergeUtils.mountMapLayoutAssignments(target);
-    var mapLayoutAssignmentsSource = mergeUtils.mountMapLayoutAssignments(source);
-    //  CustomMetadataTypeAccesses MAPS
-    var mapCustomMdtAccessesTarget = mergeUtils.mountCustomMetadataTypeAccesses(target);
-    var mapCustomMdtAccessesSource = mergeUtils.mountCustomMetadataTypeAccesses(source);
-    //  CustomPermissions MAPS
-    var mapCustomPermissionsTarget = mergeUtils.mountCustomPermissions(target);
-    var mapCustomPermissionsSource = mergeUtils.mountCustomPermissions(source);
-    //  Class Accesses MAPS
-    var mapClassAccessesTarget = mergeUtils.mountClassAccesses(target);
-    var mapClassAccessesSource = mergeUtils.mountClassAccesses(source);
-    //  CustomSettingAccesses MAPS
-    var mapCustomSettingsTarget = mergeUtils.mountCustomSettingAccesses(target);
-    var mapCCustomSettingsSource = mergeUtils.mountCustomSettingAccesses(source);
-    //  Application Visibilities MAPS
-    var mapApplicationVisibilitiesTarget = mergeUtils.mountApplicationVisibilities(target);
-    var mapApplicationVisibilitiesSource = mergeUtils.mountApplicationVisibilities(source);
-  
-    //  Object Permission MAPS
-    var mapObjectPermissionsTarget = mergeUtils.mountObjectPermissions(target);
-    var mapObjectPermissionsSource = mergeUtils.mountObjectPermissions(source);
-  
-  
+    var mergeObject = new MergeFile(fileName);
+    var targetFile = fileUtils.convertFile(target);
+    var typesMerged = "" ;
+
    
-    sourceFile.Profile.fieldPermissions = mergeUtils.mergeFieldPermissions(mapOfFieldObjTarget,mapOfFieldObjSource);
-    sourceFile.Profile.userPermissions = mergeUtils.mergeUserPermissions(mapUserPermissionTarget,mapUserPermissionSource); 
-    sourceFile.Profile.layoutAssignments = mergeUtils.mergeLayoutAssignments(mapLayoutAssignmentsTarget,mapLayoutAssignmentsSource); 
+    // MAPS - SOURCE
+    var mapOfFieldObjSource = mergeObject.mountMapFieldPermission(source);
+    var mapUserPermissionSource = mergeObject.mountMapUserPermission(source);
+    var mapLayoutAssignmentsSource = mergeObject.mountMapLayoutAssignments(source);
+    var mapCustomMdtAccessesSource = mergeObject.mountCustomMetadataTypeAccesses(source);
+    var mapCustomPermissionsSource = mergeObject.mountCustomPermissions(source);
+
+    
+    //  Class Accesses MAPS
+    var mapClassAccessesTarget = mergeObject.mountClassAccesses(target);
+    var mapClassAccessesSource = mergeObject.mountClassAccesses(source);
+    //  CustomSettingAccesses MAPS
+    var mapCustomSettingsTarget = mergeObject.mountCustomSettingAccesses(target);
+    var mapCCustomSettingsSource = mergeObject.mountCustomSettingAccesses(source);
+    //  Application Visibilities MAPS
+    var mapApplicationVisibilitiesTarget = mergeObject.mountApplicationVisibilities(target);
+    var mapApplicationVisibilitiesSource = mergeObject.mountApplicationVisibilities(source);
+
+    //  Object Permission MAPS
+    var mapObjectPermissionsTarget = mergeObject.mountObjectPermissions(target);
+    var mapObjectPermissionsSource = mergeObject.mountObjectPermissions(source);
+    if (mapOfFieldObjSource.size > 0) {
+      typesMerged += "Field Permissions \n";
+      targetFile.Profile.fieldPermissions = mergeObject.mergeFieldPermissions(mergeObject.mountMapFieldPermission(target), mapOfFieldObjSource);
+    }
+    if (mapUserPermissionSource.size > 0) {
+      typesMerged += "UserPermissions\n";
+      targetFile.Profile.userPermissions = mergeObject.mergeUserPermissions(mergeObject.mountMapUserPermission(target), mapUserPermissionSource)
+    }
+    if (mapLayoutAssignmentsSource.size > 0) {
+      typesMerged += "Layout Assignments\n";
+      targetFile.Profile.layoutAssignments = mergeObject.mergeLayoutAssignments(mergeObject.mountMapLayoutAssignments(target), mapLayoutAssignmentsSource);
+    }
+    if (mapCustomMdtAccessesSource.size > 0) {
+      typesMerged += "Custom Metadata Type Accesses\n";
+      targetFile.Profile.customMetadataTypeAccesses = mergeObject.mergeCustomMdtAccesses(mergeObject.mountCustomMetadataTypeAccesses(target), mapCustomMdtAccessesSource);
+    }
+    if (mapCustomPermissionsSource.size > 0) {
+      typesMerged += "Custom Permissions\n";
+      targetFile.Profile.customPermissions = mergeObject.mergeCustomPermissions(mergeObject.mountCustomPermissions(target), mapCustomPermissionsSource);
+    }
+
+    this.log("Types Merged : \n \n" + typesMerged);
+
+
+    /*  
     sourceFile.Profile.customMetadataTypeAccesses = mergeUtils.mergeCustomMdtAccesses(mapCustomMdtAccessesTarget,mapCustomMdtAccessesSource); 
     sourceFile.Profile.customPermissions = mergeUtils.mergeCustomPermissions(mapCustomPermissionsTarget,mapCustomPermissionsSource); 
     sourceFile.Profile.classAccesses = mergeUtils.mergeClassAccesses(mapClassAccessesTarget,mapClassAccessesSource); 
     sourceFile.Profile.customSettingAccesses = mergeUtils.mergeClassAccesses(mapCustomSettingsTarget,mapCCustomSettingsSource); 
     sourceFile.Profile.applicationVisibilities = mergeUtils.mergeApplicationVisibilities(mapApplicationVisibilitiesTarget,mapApplicationVisibilitiesSource); 
-    sourceFile.Profile.objectPermissions = mergeUtils.mergeObjectPermissions(mapObjectPermissionsTarget,mapObjectPermissionsSource); 
+    sourceFile.Profile.objectPermissions = mergeUtils.mergeObjectPermissions(mapObjectPermissionsTarget,mapObjectPermissionsSource);  */
+    fileUtils.writeChanges(targetFile, this.targetFolder, fileName);
 
-    mergeUtils.writeChanges(sourceFile,this.targetFolder,fileName);
+  }
 
+  get fileName(){
+    return this.fileMerging;
   }
 
 }
---
+++ src/utils/mergeFile.ts
@@ -0,0 +1,492 @@
+import * as xml2js from 'xml2js';
+const logger = require('./logUtils');
+
+
+export default class MergeFile {
+    fileName;
+    constructor(fileName: string) {
+      this.fileName = fileName;
+    }
+
+    // CREATE MAP FUNCTIONS
+ mountMapFieldPermission(file: any) {
+    var mapOfFieldPerm = new Map();
+    xml2js.parseString(file, (err: Error, result: any) => {
+      if (err) {
+        let erroMessage = "File --> " + this.fileName + "\n"  + err.message 
+        logger.error(erroMessage);
+        throw erroMessage;
+      } else {
+        var json = result;
+        if (json.Profile.fieldPermissions != null) {
+          for (let x of json.Profile.fieldPermissions) {
+            if (x.field != null) {
+              mapOfFieldPerm.set(x.field.toString(), x);
+            }
+          }
+        }
+      }
+    });
+    return mapOfFieldPerm;
+  }
+  
+   mountMapUserPermission(file: any) {
+    var mapUserPermission = new Map();
+    xml2js.parseString(file, (err: Error, result: any) => {
+      if (err) {
+        let erroMessage = "File --> " + this.fileName + "\n"  + err.message 
+        logger.error(erroMessage);
+        throw erroMessage;
+      } else {
+        var json = result;
+        if (json.Profile.userPermissions != null) {
+          for (let usrPerm of json.Profile.userPermissions) {
+            if (usrPerm.name != null) {
+              mapUserPermission.set(usrPerm.name.toString(), usrPerm);
+            }
+          }
+        }
+      }
+    });
+    return mapUserPermission;
+  }
+  
+   mountMapLayoutAssignments(file: any) {
+    var mapOfLayoutAssignments = new Map();
+    xml2js.parseString(file, (err: Error, result: any) => {
+      if (err) {
+        let erroMessage = "File --> " + this.fileName + "\n"  + err.message 
+        logger.error(erroMessage);
+        throw erroMessage;
+      } else {
+        var json = result;
+        if (json.Profile.layoutAssignments != null) {
+          for (let x of json.Profile.layoutAssignments) {
+            if (x.layout != null) {
+              mapOfLayoutAssignments.set(x.layout.toString(), x);
+            }
+          }
+        }
+      }
+    });
+    return mapOfLayoutAssignments;
+  }
+  
+   mountCustomMetadataTypeAccesses(file: any) {
+    var mapOfCustomMdtAccess = new Map();
+    xml2js.parseString(file, (err: Error, result: any) => {
+      if (err) {
+        let erroMessage = "File --> " + this.fileName + "\n"  + err.message 
+        logger.error(erroMessage);
+        throw erroMessage;
+      } else {
+        var json = result;
+        if (json.Profile.customMetadataTypeAccesses != null) {
+          for (let obj of json.Profile.customMetadataTypeAccesses) {
+            if (obj.name != null) {
+              mapOfCustomMdtAccess.set(obj.name.toString(), obj);
+            }
+          }
+        }
+      }
+    });
+    return mapOfCustomMdtAccess;
+  }
+  
+   mountCustomPermissions(file: any) {
+    var mapOfCustomPermission = new Map();
+    xml2js.parseString(file, (err: Error, result: any) => {
+      if (err) {
+        let erroMessage = "File --> " + this.fileName + "\n"  + err.message 
+        logger.error(erroMessage);
+        throw erroMessage;
+      } else {
+        var json = result;
+        if (json.Profile.customPermissions != null) {
+          for (let obj of json.Profile.customPermissions) {
+            if (obj.name != null) {
+              mapOfCustomPermission.set(obj.name.toString(), obj);
+            }
+          }
+        }
+      }
+    });
+    return mapOfCustomPermission;
+  }
+  
+   mountClassAccesses(file: any) {
+    var mapOfClassAccesses = new Map();
+    xml2js.parseString(file, (err: Error, result: any) => {
+      if (err) {
+        let erroMessage = "File --> " + this.fileName + "\n"  + err.message 
+        logger.error(erroMessage);
+        throw erroMessage;
+      } else {
+        var json = result;
+        if (json.Profile.classAccesses != null) {
+          for (let obj of json.Profile.classAccesses) {
+            if (obj.apexClass != null) {
+              mapOfClassAccesses.set(obj.apexClass.toString(), obj);
+            }
+          }
+        }
+      }
+    });
+    return mapOfClassAccesses;
+  }
+  
+   mountCustomSettingAccesses(file: any) {
+    var mapOfCustomSettings = new Map();
+    xml2js.parseString(file, (err: Error, result: any) => {
+      if (err) {
+        let erroMessage = "File --> " + this.fileName + "\n"  + err.message 
+        logger.error(erroMessage);
+        throw erroMessage;
+      } else {
+        var json = result;
+        if (json.Profile.customSettingAccesses != null) {
+          for (let obj of json.Profile.customSettingAccesses) {
+            if (obj.name != null) {
+              mapOfCustomSettings.set(obj.name.toString(), obj);
+            }
+          }
+        }
+      }
+    });
+    return mapOfCustomSettings;
+  
+  
+  }
+   mountApplicationVisibilities(file: any) {
+    var mapOfApplicationVisibilities = new Map();
+    xml2js.parseString(file, (err: Error, result: any) => {
+      if (err) {
+        let erroMessage = "File --> " + this.fileName + "\n"  + err.message 
+        logger.error(erroMessage);
+        throw erroMessage;
+      } else {
+        var json = result;
+        if (json.Profile.applicationVisibilities != null) {
+          for (let obj of json.Profile.applicationVisibilities) {
+            if (obj.application != null) {
+              mapOfApplicationVisibilities.set(obj.application.toString(), obj);
+            }
+          }
+        }
+      }
+    });
+    return mapOfApplicationVisibilities;
+  }
+  
+   mountObjectPermissions(file: any) {
+    var mapOfObjPermissions = new Map();
+    xml2js.parseString(file, (err: Error, result: any) => {
+      if (err) {
+        let erroMessage = "File --> " + this.fileName + "\n"  + err.message 
+        logger.error(erroMessage);
+        throw erroMessage;
+      } else {
+        var json = result;
+        if (json.Profile.objectPermissions != null) {
+          for (let obj of json.Profile.objectPermissions) {
+            if (obj.object != null) {
+              mapOfObjPermissions.set(obj.object.toString(), obj);
+            }
+          }
+        }
+      }
+    });
+    return mapOfObjPermissions;
+  }
+  
+   mountLoginFlows(file: any) {
+    var mapOfLoginFlows = new Map();
+    xml2js.parseString(file, (err: Error, result: any) => {
+      if (err) {
+        let erroMessage = "File --> " + this.fileName + "\n"  + err.message 
+        logger.error(erroMessage);
+        throw erroMessage;
+      } else {
+        var json = result;
+        if (json.Profile.loginFlows != null) {
+          for (let obj of json.Profile.loginFlows) {
+            if (obj.friendlyName) {
+              mapOfLoginFlows.set(obj.friendlyName.toString(), obj);
+            }
+          }
+        }
+      }
+    });
+    return mapOfLoginFlows;
+  }
+  
+   mountPageAccess(file: any) {
+    var mapOfLoginFlows = new Map();
+    xml2js.parseString(file, (err: Error, result: any) => {
+      if (err) {
+        let erroMessage = "File --> " + this.fileName + "\n"  + err.message 
+        logger.error(erroMessage);
+        throw erroMessage;
+      } else {
+        var json = result;
+        if (json.Profile.pageAccesses != null) {
+          for (let page of json.Profile.pageAccesses) {
+            if (page.apexPage != null) {
+              mapOfLoginFlows.set(page.apexPage.toString(), page);
+            }
+          }
+        }
+      }
+    });
+    return mapOfLoginFlows;
+  }
+  
+  // MERGE FUNCTIONS
+  
+   mergeFieldPermissions(mapOfFieldObjTarget: Map<any, any>, mapOfFieldObjSource: Map<any, any>) {
+    var arrayFieldPermission = new Array();
+  
+    for (let field of mapOfFieldObjTarget.keys()) {
+      if (mapOfFieldObjSource.has(field) == true) {
+        var targetField = mapOfFieldObjTarget.get(field);
+        targetField.editable = mapOfFieldObjSource.get(field).editable;
+        targetField.readable = mapOfFieldObjSource.get(field).readable;
+        arrayFieldPermission.push(targetField);
+      } else {
+        arrayFieldPermission.push(mapOfFieldObjTarget.get(field));
+      }
+    }
+  
+    for (let field of mapOfFieldObjSource.keys()) {
+      if (mapOfFieldObjTarget.has(field) == true) {
+        continue;
+      } else {
+        arrayFieldPermission.push(mapOfFieldObjSource.get(field));
+      }
+    }
+    return arrayFieldPermission;
+  
+  }
+  
+   mergeUserPermissions(mapUserPermissionTarget: Map<any, any>, mapUserPermissionSource: Map<any, any>) {
+    var arrayUserPermission = new Array();
+    for (let userPermission of mapUserPermissionTarget.keys()) {
+      if (mapUserPermissionSource.has(userPermission) == true) {
+        var targetUsrPerm = mapUserPermissionTarget.get(userPermission);
+        targetUsrPerm.enabled = mapUserPermissionSource.get(userPermission).enabled;
+        arrayUserPermission.push(targetUsrPerm);
+      } else {
+        arrayUserPermission.push(mapUserPermissionTarget.get(userPermission));
+      }
+    }
+  
+    for (let field of mapUserPermissionSource.keys()) {
+      if (mapUserPermissionTarget.has(field) == true) {
+        continue;
+      } else {
+        arrayUserPermission.push(mapUserPermissionSource.get(field));
+      }
+    }
+  
+  
+    return arrayUserPermission;
+  
+  }
+  
+  // SPECIFIC INFORMATIONS ( CASE THE TARGET FILE HAS THE PERMISSION, ONLY VERIFIY IF CHANGE RECORDTYPE)
+   mergeLayoutAssignments(mapLayoutAssignmentsTarget: Map<any, any>, mapLayoutAssignmentsSource: Map<any, any>) {
+    var arrayLayoutAssigments = new Array();
+    for (let layout of mapLayoutAssignmentsTarget.keys()) {
+      if (mapLayoutAssignmentsSource.has(layout) == true) {
+        var targetLayoutPerm = mapLayoutAssignmentsTarget.get(layout);
+        if (targetLayoutPerm.recordType != null && mapLayoutAssignmentsSource.get(layout).recordType == null) {
+          delete targetLayoutPerm.recordType;
+          arrayLayoutAssigments.push(targetLayoutPerm);
+        } else if (targetLayoutPerm.recordType == null && mapLayoutAssignmentsSource.get(layout).recordType != null) {
+          targetLayoutPerm.recordType = mapLayoutAssignmentsSource.get(layout).recordType;
+          arrayLayoutAssigments.push(targetLayoutPerm);
+        } else {
+          arrayLayoutAssigments.push(targetLayoutPerm);
+        }
+      } else {
+        arrayLayoutAssigments.push(mapLayoutAssignmentsTarget.get(layout));
+      }
+    }
+  
+    for (let field of mapLayoutAssignmentsSource.keys()) {
+      if (mapLayoutAssignmentsTarget.has(field) == true) {
+        continue;
+      } else {
+        arrayLayoutAssigments.push(mapLayoutAssignmentsSource.get(field));
+      }
+    }
+    return arrayLayoutAssigments;
+  }
+  
+   mergeCustomMdtAccesses(mapUserPermissionTarget: Map<any, any>, mapUserPermissionSource: Map<any, any>) {
+    var arrayCustomMdtAccesses = new Array();
+    for (let name of mapUserPermissionTarget.keys()) {
+      if (mapUserPermissionSource.has(name) == true) {
+        var targetCustomMdt = mapUserPermissionTarget.get(name);
+        targetCustomMdt.enabled = mapUserPermissionSource.get(name).enabled;
+        arrayCustomMdtAccesses.push(targetCustomMdt);
+      } else {
+        arrayCustomMdtAccesses.push(mapUserPermissionTarget.get(name));
+      }
+    }
+  
+    for (let name of mapUserPermissionSource.keys()) {
+      if (mapUserPermissionTarget.has(name) == true) {
+        continue;
+      } else {
+        arrayCustomMdtAccesses.push(mapUserPermissionSource.get(name));
+      }
+    }
+  
+    return arrayCustomMdtAccesses;
+  
+  }
+  
+   mergeCustomPermissions(mapCustomPermissionTarget: Map<any, any>, mapCustomPermissionSource: Map<any, any>) {
+    var arrayCustomPermission = new Array();
+    
+    for (let name of mapCustomPermissionTarget.keys()) {
+      if (mapCustomPermissionSource.has(name) == true) {
+        var targetCustomMdt = mapCustomPermissionTarget.get(name);
+        targetCustomMdt.enabled = mapCustomPermissionSource.get(name).enabled;
+        arrayCustomPermission.push(targetCustomMdt);
+      } else {
+        arrayCustomPermission.push(mapCustomPermissionTarget.get(name));
+      }
+    }
+  
+    for (let name of mapCustomPermissionSource.keys()) {
+      if (mapCustomPermissionTarget.has(name) == true) {
+        continue;
+      } else {
+        arrayCustomPermission.push(mapCustomPermissionSource.get(name));
+      }
+    }
+  
+    return arrayCustomPermission;
+  
+  }
+  
+   mergeClassAccesses(mapClassAccessesTarget: Map<any, any>, mapUserClassAccessesSource: Map<any, any>) {
+    var arrayClassAccesses = new Array();
+    for (let apexClass of mapUserClassAccessesSource.keys()) {
+      if (mapClassAccessesTarget.has(apexClass) == true) {
+        var targetClassAccesses = mapClassAccessesTarget.get(apexClass);
+        targetClassAccesses.enabled = mapUserClassAccessesSource.get(apexClass).enabled;
+        arrayClassAccesses.push(targetClassAccesses);
+      } else {
+        arrayClassAccesses.push(mapUserClassAccessesSource.get(apexClass.toString()));
+      }
+    }
+  
+    return arrayClassAccesses;
+  
+  }
+  
+   mergeCustomSettings(mapCustomSettingsTarget: Map<any, any>, mapCustomSettingsSource: Map<any, any>) {
+    var arrayCustomSettings = new Array();
+    for (let customSettings of mapCustomSettingsSource.keys()) {
+      if (mapCustomSettingsTarget.has(customSettings) == true) {
+        var targetClassAccesses = mapCustomSettingsTarget.get(customSettings);
+        targetClassAccesses.enabled = mapCustomSettingsSource.get(customSettings).enabled;
+        arrayCustomSettings.push(targetClassAccesses);
+      } else {
+        arrayCustomSettings.push(mapCustomSettingsSource.get(customSettings.toString()));
+      }
+    }
+  
+    return arrayCustomSettings;
+  
+  }
+  
+   mergeApplicationVisibilities(mapAppVisibilitiesTarget: Map<any, any>, mapAppVisibilitiesSource: Map<any, any>) {
+    var arrayAppVisibilities = new Array();
+    for (let appVisibilities of mapAppVisibilitiesSource.keys()) {
+      if (mapAppVisibilitiesTarget.has(appVisibilities) == true) {
+        var app = mapAppVisibilitiesTarget.get(appVisibilities);
+        app.default = mapAppVisibilitiesSource.get(appVisibilities).default;
+        app.visible = mapAppVisibilitiesSource.get(appVisibilities).visible;
+        arrayAppVisibilities.push(app);
+      } else {
+        arrayAppVisibilities.push(mapAppVisibilitiesSource.get(appVisibilities.toString()));
+      }
+    }
+  
+    return arrayAppVisibilities;
+  
+  }
+  
+   mergeObjectPermissions(mapObjPermTarget: Map<any, any>, mapObjPermSource: Map<any, any>) {
+    var arrayObjPermissions = new Array();
+    for (let objtPermission of mapObjPermSource.keys()) {
+      if (mapObjPermTarget.has(objtPermission) == true) {
+        var objtTarget = mapObjPermTarget.get(objtPermission);
+        objtTarget.allowCreate = mapObjPermSource.get(objtPermission).allowCreate;
+        objtTarget.allowDelete = mapObjPermSource.get(objtPermission).allowDelete;
+        objtTarget.allowEdit = mapObjPermSource.get(objtPermission).allowEdit;
+        objtTarget.allowRead = mapObjPermSource.get(objtPermission).allowRead;
+        objtTarget.modifyAllRecords = mapObjPermSource.get(objtPermission).modifyAllRecords;
+        objtTarget.viewAllRecords = mapObjPermSource.get(objtPermission).viewAllRecords;
+        arrayObjPermissions.push(objtTarget);
+      } else {
+        arrayObjPermissions.push(mapObjPermSource.get(objtPermission.toString()));
+      }
+    }
+  
+    return arrayObjPermissions;
+  
+  }
+  // SPECIFIC INFORMATIONS ( OBTAIN CHANGES ONLY IN THE SAME TYPE) - IF CHANGE TYPE, IS NEEDLY MANUAL ACTIONS
+   mergeLoginFlows(mapLoginFlowsTarget: Map<any, any>, mapLoginFlowsSource: Map<any, any>) {
+    var arrayLoginFlows = new Array();
+    for (let loginFlows of mapLoginFlowsSource.keys()) {
+      if (mapLoginFlowsTarget.has(loginFlows) == true) {
+        var loginFlowsObj = mapLoginFlowsTarget.get(loginFlows);
+        if (mapLoginFlowsTarget.get(loginFlows).uiLoginFlowType.toString() == mapLoginFlowsSource.get(loginFlows).uiLoginFlowType.toString()) {
+  
+          if (loginFlowsObj.uiLoginFlowType == 'VisualForce') {
+            loginFlowsObj.vfFlowPage = mapLoginFlowsSource.get(loginFlows).vfFlowPage;
+            loginFlowsObj.vfFlowPageTitle = mapLoginFlowsSource.get(loginFlows).vfFlowPageTitle;
+            loginFlowsObj.useLightningRuntime = mapLoginFlowsSource.get(loginFlows).useLightningRuntime;
+  
+          } else if (loginFlowsObj.uiLoginFlowType == 'VisualWorkflow') {
+            loginFlowsObj.flow = mapLoginFlowsSource.get(loginFlows).flow;
+            loginFlowsObj.useLightningRuntime = mapLoginFlowsSource.get(loginFlows).useLightningRuntime;
+  
+          }
+        }
+  
+        arrayLoginFlows.push(loginFlowsObj);
+  
+      } else {
+  
+        arrayLoginFlows.push(mapLoginFlowsSource.get(loginFlows.toString()));
+  
+      }
+    }
+  
+    return arrayLoginFlows;
+  
+  }
+  
+   mergePageAccesses(mapPageAccessesTarget: Map<any, any>, mapPageAccessesSource: Map<any, any>) {
+    var arrayCustomSettings = new Array();
+    for (let page of mapPageAccessesSource.keys()) {
+      if (mapPageAccessesTarget.has(page) == true) {
+        var targetClassAccesses = mapPageAccessesTarget.get(page);
+        targetClassAccesses.enabled = mapPageAccessesSource.get(page).enabled;
+        arrayCustomSettings.push(targetClassAccesses);
+      } else {
+        arrayCustomSettings.push(mapPageAccessesSource.get(page.toString()));
+      }
+    }
+  
+    return arrayCustomSettings;
+  
+  }
+}
